28875704fab6e09b0d3aaca9c63ea3c2cb0f2620
RafaelBP02 <rafaelbertopereira@gmail.com>
2024-10-15 16:47:29 -0300

    feat: add button to navigate to homepage

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { Image, StyleSheet, Platform, Button } from 'react-native';
 
 import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
@@ -6,6 +6,10 @@ import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 
 export default function HomeScreen() {
+  function goToHome():void {
+    console.log('deve mudar de pagina')
+  }
+
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
@@ -23,15 +27,22 @@ export default function HomeScreen() {
         <ThemedText type="subtitle">O seu notificador de pontos</ThemedText>
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle"> Get a fresh start</ThemedText>
+        <ThemedText type="subtitle"> Get a fresh start TESTE</ThemedText>
         <ThemedText>
-          When you're ready, run{' '}
+          When you're ready, run{' '} teste
           <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
           <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
           <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
           <ThemedText type="defaultSemiBold">app-example</ThemedText>.
         </ThemedText>
+        <Button
+        onPress={goToHome}
+        title="entrar"
+        color="#841584"
+
+      />
       </ThemedView>
+      
     </ParallaxScrollView>
   );
 }
