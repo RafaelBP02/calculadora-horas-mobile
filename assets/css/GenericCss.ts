1d17a8d70d8ba987b5c2def636663d2e28ef3bfe
RafaelBP02 <rafaelbertopereira@gmail.com>
2024-10-18 11:07:02 -0300

    style: create a generic style container

---
+++ assets/css/GenericCss.ts
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+
+const genericCss = StyleSheet.create({
+    container: {
+        flexDirection: 'column',
+        width: '100%',
+        height: '100%',
+
+        justifyContent: 'center',
+        alignContent:'center',
+        alignItems:'center'
+    }
+})
+
+export {genericCss};
