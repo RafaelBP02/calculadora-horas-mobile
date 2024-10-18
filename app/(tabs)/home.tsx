a9edaadffd8fec3c18b2ba9573619765403f1e29
RafaelBP02 <rafaelbertopereira@gmail.com>
2024-10-18 16:58:07 -0300

    style: update home screen

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,17 +1,26 @@
+import { formCss } from "@/assets/css/FormsCss";
+import { genericCss } from "@/assets/css/GenericCss";
 import { RootStackParamList } from "@/utils/customTypes";
 import React from "react";
 import { Button, Text, View } from "react-native";
 import { NativeStackScreenProps } from "react-native-screens/lib/typescript/native-stack/types";
 
-type HomeProps = NativeStackScreenProps<RootStackParamList, 'Home'>;
+type HomeProps = NativeStackScreenProps<RootStackParamList, "Home">;
 
-export default function Home({navigation}: HomeProps){
-    
-    
-    return(
-        <View>
-            <Text>Calcule seu horario</Text>
-            <Button title="calculadora" onPress={() => navigation.navigate('Calculator')}></Button>
-        </View>
-    )
+export default function Home({ navigation }: HomeProps) {
+  return (
+    <View style={genericCss.container}>
+      <Text>BEM VINDO À CALCULADORA DE HORAS</Text>
+      <View style={formCss.button}>
+        <Button
+          title="calculadora"
+          onPress={() => navigation.navigate("Calculator")}
+        ></Button>
+        <Button
+          title="login"
+          onPress={() => navigation.navigate("Login")}
+        ></Button>
+      </View>
+    </View>
+  );
 }
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,6 +2,7 @@ import { RootStackParamList } from "@/utils/customTypes";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Home from "./home";
 import Calculator from "./calculator";
+import Login from "./login";
 
 export default function HomeScreen() {
   const Stack = createNativeStackNavigator<RootStackParamList>();
@@ -10,7 +11,7 @@ export default function HomeScreen() {
     <Stack.Navigator>
       <Stack.Screen name="Home" component={Home} />
       <Stack.Screen name="Calculator"  component={Calculator} />
-      {/* <Stack.Screen name="Login" options={{headerShown: false}} component={Login} /> */}
+      <Stack.Screen name="Login" options={{headerShown: false}} component={Login} />
     </Stack.Navigator>
   );
 }
