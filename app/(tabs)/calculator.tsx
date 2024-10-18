7f50962cad77428cd778ee83c9cde05e6aadacf4
RafaelBP02 <rafaelbertopereira@gmail.com>
2024-10-18 11:07:22 -0300

    style: config form styling

--- app/(tabs)/calculator.tsx
+++ app/(tabs)/calculator.tsx
@@ -1,3 +1,5 @@
+import { colors, formCss } from "@/assets/css/FormsCss";
+import { genericCss } from "@/assets/css/GenericCss";
 import { Conversions } from "@/utils/Conversions";
 import React, { useState } from "react";
 import {
@@ -23,67 +25,91 @@ export default function Calculator() {
     // const dataFE = Conversions.parseTimeString(fimExpediente);
 
     // incluir || dataFE === null
-    if (dataIE === null || dataII === null ||  dataFI === null) {
+    if (dataIE === null || dataII === null || dataFI === null) {
       Alert.alert(
-        "Formato de hora inválido", "Por favor, apenas números, insira a hora no formato HH:MM."
+        "Formato de hora inválido",
+        "Por favor, apenas números, insira a hora no formato HH:MM."
       );
 
       return;
-    } 
+    }
 
+    console.log("sucesso dados validados");
 
-    console.log("sucesso dados validados")
-
-    //TODO: funcao para calcular a diferenca de horas
     calculaHorarioSaida(dataIE, dataII, dataFI);
   };
 
-  const calculaHorarioSaida = (horaEntrada: Date, inicioIntervalo: Date, fimIntervalo: Date) => {
+  const calculaHorarioSaida = (
+    horaEntrada: Date,
+    inicioIntervalo: Date,
+    fimIntervalo: Date
+  ) => {
     const cargaHoraria: number = 8;
 
-    const horasJaTrabalhadas:number = inicioIntervalo.getTime() - horaEntrada.getTime();
+    const horasJaTrabalhadas: number =
+      inicioIntervalo.getTime() - horaEntrada.getTime();
 
-    const horarioDeSaida:Date = new Date(fimIntervalo.getTime() + cargaHoraria * 60 * 60 * 1000 - horasJaTrabalhadas)
-  
-    Alert.alert("Horario de Saída", horarioDeSaida.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: false }));
+    const horarioDeSaida: Date = new Date(
+      fimIntervalo.getTime() +
+        cargaHoraria * 60 * 60 * 1000 -
+        horasJaTrabalhadas
+    );
+
+    Alert.alert(
+      "Horario de Saída",
+      horarioDeSaida.toLocaleTimeString([], {
+        hour: "2-digit",
+        minute: "2-digit",
+        hour12: false,
+      })
+    );
+  };
+
+  const limparHorarios = () => {
+    setInicioExpediente("");
+    setInicioIntervalo("");
+    setFimIntervalo("");
   }
 
   return (
     <KeyboardAvoidingView
       behavior={Platform.OS == "ios" ? "padding" : "height"}
     >
-      <View>
-        <View>
-          <Text>Defina seus Horarios (8 horas de carga horaria)</Text>
-        </View>
-        <View>
-          <TextInput
-            value={inicioExpediente}
-            placeholder="Hora início expediente (HH:MM)"
-            onChangeText={(text) => setInicioExpediente(text)}
-            keyboardType="numeric"
-          />
-          <TextInput
-            value={inicioIntervalo}
-            placeholder="Hora início intervalo (HH:MM)"
-            onChangeText={(text) => setInicioIntervalo(text)}
-            keyboardType="numeric"
-          />
-          <TextInput
-            value={fimIntervalo}
-            placeholder="Hora fim intervalo (HH:MM)"
-            onChangeText={(text) => setFimIntervalo(text)}
-            keyboardType="numeric"
-          />
-          {/* <TextInput
+      <View style={genericCss.container}>
+        <View style={formCss.formContainer}>
+          <View>
+            <Text>Defina seus Horarios (8 horas de carga horaria)</Text>
+          </View>
+          <View style={formCss.formItems}>
+            <TextInput
+              value={inicioExpediente}
+              placeholder="Hora início expediente (HH:MM)"
+              onChangeText={(text) => setInicioExpediente(text)}
+              keyboardType="numeric"
+            />
+            <TextInput
+              value={inicioIntervalo}
+              placeholder="Hora início intervalo (HH:MM)"
+              onChangeText={(text) => setInicioIntervalo(text)}
+              keyboardType="numeric"
+            />
+            <TextInput
+              value={fimIntervalo}
+              placeholder="Hora fim intervalo (HH:MM)"
+              onChangeText={(text) => setFimIntervalo(text)}
+              keyboardType="numeric"
+            />
+            {/* <TextInput
             value={fimExpediente}
             placeholder="Hora fim expediente (HH:MM)"
             onChangeText={(text) => setFimExpediente(text)}
             keyboardType="numeric"
           /> */}
-        </View>
-        <View>
-          <Button title="calcular" onPress={() => handleTimeSend()} />
+          </View>
+          <View style={formCss.button}>
+            <Button title="calcular" color={colors.primary} onPress={() => handleTimeSend()} />
+            <Button title="cancelar" color={colors.secondary} onPress={() => limparHorarios()} />
+          </View>
         </View>
       </View>
     </KeyboardAvoidingView>
