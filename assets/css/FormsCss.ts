e9c98c066a4220621747b4a7afbde7be43927475
RafaelBP02 <rafaelbertopereira@gmail.com>
2024-10-18 11:07:38 -0300

    style: apply created styles

--- assets/css/FormsCss.ts
+++ assets/css/FormsCss.ts
@@ -1,33 +1,42 @@
 import { StyleSheet } from "react-native";
 
 const colors = {
-    primary: '#3498db',
-    secondary: '#2ecc71',
-    background: '#ecf0f1',
-    text: '#2c3e50',
-    white: '#ffffff',
-    black: '#000000',
-  };
-  
-  const spacing = {
-    small: 8,
-    medium: 16,
-    large: 24,
-  };
-  
-  const fontSizes = {
-    small: 12,
-    medium: 16,
-    large: 20,
-  };
+  primary: "#407cff",
+  secondary: "#9da2a6",
+  text: "#2c3e50",
+  white: "#ffffff",
+  black: "#000000",
+};
+
+const spacing = {
+  small: 8,
+  medium: 16,
+  large: 24,
+};
+
+const fontSizes = {
+  small: 12,
+  medium: 16,
+  large: 20,
+};
 
 const formCss = StyleSheet.create({
-    button: {
-        backgroundColor: colors.primary,
-        padding: spacing.medium,
-        borderRadius: 5,
-        alignItems: 'center',
-      },
-}) 
+  button: {
+    width:'90%',
+    gap: 10,
+    marginBottom: 10,
+  },
+  formContainer: {
+    width: "80%",
 
-export {formCss}
+    alignItems: "center",
+    gap: 30,
+    backgroundColor: "#f0f0f0",
+  },
+  formItems: {
+    width:'70%',
+    gap: 10,
+  },
+});
+
+export { formCss, colors };
