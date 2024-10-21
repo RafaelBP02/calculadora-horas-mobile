6705d1610859f9620ac4e576591dc80b3dc217d6
RafaelBP02 <rafaelbertopereira@gmail.com>
2024-10-21 12:56:57 -0300

    feat: create functional login form

--- app/(tabs)/login.tsx
+++ app/(tabs)/login.tsx
@@ -1,3 +1,4 @@
+import { formCss } from "@/assets/css/FormsCss";
 import { genericCss } from "@/assets/css/GenericCss";
 import React, { useState } from "react";
 import {
@@ -9,19 +10,57 @@ import {
   TextInput,
   View,
 } from "react-native";
+import { UserAuthentication } from "../controller/authenticationController";
+import { BearerToken } from "../models/authenticationModel";
 
 export default function Login() {
-  const [usuario, setUsuario] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
   const [senha, setSenha] = useState<string>("");
+  const [dadosValidos, setDadosValidos] = useState<boolean>(false);
 
-    
+  const tratarEnvio = async () => {
+    if (!email.includes("@") || !email.includes(".com")) {
+      setDadosValidos(false);
+      return;
+    }
+    console.log("ENVIADO!");
+
+    try {
+      const userToken = await UserAuthentication.login(email, senha);
+      console.log(userToken.token);
+  
+    } catch (error) {
+      console.error("Erro ao fazer login:", error);
+    }
+
+  };
 
   return (
     <KeyboardAvoidingView
       behavior={Platform.OS == "ios" ? "padding" : "height"}
     >
       <View style={genericCss.container}>
-        
+        <View style={formCss.formContainer}>
+        <View>
+            <Text>LOGIN</Text>
+          </View>
+          <TextInput
+            style={formCss.formInput}
+            placeholder="usuaruio@mail.com"
+            value={email}
+            onChangeText={setEmail}
+          />
+          <TextInput
+            style={formCss.formInput}
+            placeholder="digite sua senha"
+            value={senha}
+            onChangeText={setSenha}
+            secureTextEntry={true}
+          />
+          <View style={formCss.button}>
+            <Button title="Enviar" onPress={() => tratarEnvio()} />
+          </View>
+        </View>
       </View>
     </KeyboardAvoidingView>
   );
