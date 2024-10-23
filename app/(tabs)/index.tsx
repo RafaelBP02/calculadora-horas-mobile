e36faf8ce809e90347e9bb60f6ccf9cf52749e1f
RafaelBP02 <rafaelbertopereira@gmail.com>
2024-10-23 11:00:18 -0300

    feat: add custom header

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,18 +4,46 @@ import Home from "../views/home";
 import Calculator from "./calculator";
 import Login from "../views/login";
 import { AuthContextProvider } from "@/contexts/Auth";
+import LoggedInHeader from "@/components/LoggedInHeader";
 
 export default function HomeScreen() {
   const Stack = createNativeStackNavigator<RootStackParamList>();
 
+
   return (
     <AuthContextProvider>
       <Stack.Navigator>
-          <Stack.Screen name="Home" component={Home} />
-          <Stack.Screen name="Calculator"  component={Calculator} />
-          <Stack.Screen name="Login" options={{headerShown: false}} component={Login} />
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{
+            headerRight: () => (
+              <LoggedInHeader
+                title="Info"
+                onPress={() => alert('This is a button!')}
+              />
+            ),
+          }}
+        />
+        <Stack.Screen
+          name="Calculator"
+          component={Calculator}
+          options={{
+            headerRight: () => (
+              <LoggedInHeader
+                title="Info"
+                onPress={() => alert('This is a button!')}
+              />
+            ),
+          }}
+          
+        />
+        <Stack.Screen
+          name="Login"
+          options={{ headerShown: false }}
+          component={Login}
+        />
       </Stack.Navigator>
     </AuthContextProvider>
   );
 }
-
