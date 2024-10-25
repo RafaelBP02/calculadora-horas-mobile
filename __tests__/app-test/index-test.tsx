1a89f61b107d5f5679ad8d07ee62a13e117a6d57
RafaelBP02 <rafaelbertopereira@gmail.com>
2024-10-25 16:59:27 -0300

    test: index

---
+++ __tests__/app-test/index-test.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { fireEvent, render } from "@testing-library/react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import HomeScreen from "@/app/(tabs)";
+import Calculator from "@/app/(tabs)/calculator";
+
+jest.mock("@/app/views/home", () => () => <div>Home</div>);
+jest.mock("@/app/views/login", () => () => <div>Login</div>);
+jest.mock("@/app/views/registration", () => () => <div>Registration</div>);
+jest.mock("@/app/(tabs)/calculator", () => () => <div>Calculator</div>);
+jest.mock("@/components/LoggedInHeader", () => () => <div>LoggedInHeader</div>);
+
+describe("HomeScreen Navigation", () => {
+  xit("should render the initial route as Home", () => {
+    const { getByText } = render(
+      <NavigationContainer>
+        <HomeScreen />
+      </NavigationContainer>
+    );
+
+    expect(getByText("Home")).toBeTruthy();
+  });
+
+  xit("should render Calculator screen", () => {
+    const { getByText, queryByText } = render(
+      <NavigationContainer>
+        <HomeScreen />
+      </NavigationContainer>
+    );
+
+    fireEvent.press(getByText("Calculator"));
+
+    expect(getByText("Calculator")).toBeTruthy();
+
+    expect(queryByText("LoggedInHeader")).toBeTruthy();
+  });
+
+  it("should not show LoggedInHeader in Login and Registration screens", () => {
+    const { queryByText, rerender } = render(
+      <NavigationContainer>
+        <HomeScreen />
+      </NavigationContainer>
+    );
+
+    rerender(
+      <NavigationContainer>
+        <HomeScreen />
+      </NavigationContainer>
+    );
+    expect(queryByText("Home")).toBeNull();
+
+    rerender(
+      <NavigationContainer>
+        <HomeScreen />
+      </NavigationContainer>
+    );
+    expect(queryByText("Home")).toBeNull();
+  });
+});
