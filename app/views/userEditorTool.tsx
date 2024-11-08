8078cc87fd8c7aa93dc9617a4720e593fba39a1b
RafaelBP02 <rafaelbertopereira@gmail.com>
2024-11-08 17:59:27 -0300

    feat: add userEditorTool component

---
+++ app/views/userEditorTool.tsx
@@ -0,0 +1,20 @@
+import { genericCss } from "@/assets/css/GenericCss";
+import { RootStackParamList } from "@/constants/customTypes";
+import { NativeStackScreenProps } from "react-native-screens/lib/typescript/native-stack/types";
+import { CustomUser } from "../models/userModel";
+import { Text, View } from "react-native";
+import { TextInput } from "react-native-gesture-handler";
+
+type NavProps = NativeStackScreenProps<RootStackParamList, "UserEditorTool">;
+
+
+export default function UserEditorTool({ navigation }: NavProps, selectedUser: CustomUser) {
+    return(
+        <View style={genericCss.container}>
+            <Text>Dados de {selectedUser.name} {selectedUser.surename}</Text>
+            <Text>e-mail: {selectedUser.eMail}</Text>
+            <Text>permissão: {selectedUser.role.roleName}</Text>
+            <TextInput value={selectedUser.workplace}>local de trabalho:</TextInput>
+        </View>
+    )
+}
--- constants/customTypes.ts
+++ constants/customTypes.ts
@@ -4,4 +4,5 @@ export type RootStackParamList = {
     Login: undefined;
     Registration: undefined;
     EditUsers: undefined;
+    UserEditorTool: undefined;
   };
