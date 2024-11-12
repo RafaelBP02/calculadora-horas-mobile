331d249d3ddf9971c300d8bfd81284e0a1a0aa10
RafaelBP02 <rafaelbertopereira@gmail.com>
2024-11-12 11:53:48 -0300

    feat: trasfer selected user data to admin editor

--- app/views/editUsers.tsx
+++ app/views/editUsers.tsx
@@ -10,7 +10,7 @@ import {
   ActivityIndicator,
   FlatList,
 } from "react-native";
-import { NativeStackScreenProps } from "react-native-screens/lib/typescript/native-stack/types";
+import { NativeStackNavigationProp, NativeStackScreenProps } from "react-native-screens/lib/typescript/native-stack/types";
 import { CustomUser } from "../models/userModel";
 import { AdministrationController } from "../controller/administrationController";
 import TokenContext from "@/contexts/Token";
@@ -84,7 +84,7 @@ export default function EditUsers({ navigation }: NavProps) {
           <FlatList
             testID="flatlist"
             data={listToRender}
-            renderItem={({ item }) => <ListItem data={item}/>}
+            renderItem={({ item }) => <ListItem data={item} navigation={navigation}/>}
             keyExtractor={(item) => item.id.toString()}
             onEndReached={() => updateListRendering(allUsers)}
             onEndReachedThreshold={0.5}
@@ -100,13 +100,18 @@ export default function EditUsers({ navigation }: NavProps) {
   );
 }
 
-function ListItem({data}: {data:CustomUser}) {
+type ListItemProps = {
+  data: CustomUser,
+  navigation: NativeStackNavigationProp<RootStackParamList, 'EditUsers'>
+};
+
+function ListItem({ data, navigation}: ListItemProps) {
   return (
     <View style={tableCss.row}>
       <Text style={tableCss.cell}>{data.name || 'Nome não disponível'}</Text>
       <Text style={tableCss.cell}>{data.workplace || 'Local de trabalho não disponível'}</Text>
-      <TouchableOpacity onPress={() => console.log(`usuario: ${data.name, data.id}`)}>
-        <Ionicons testID="pencilBtn" name="pencil" size={20} color={colors.primary} />
+      <TouchableOpacity onPress={() => navigation.navigate('UserEditorTool', {selectedUser: data})}>
+        <Ionicons testID="pencilBtn" name="pencil" size={20} color={colors.primary}/>
       </TouchableOpacity>
     </View>
   );
--- app/views/userEditorTool.tsx
+++ app/views/userEditorTool.tsx
@@ -8,13 +8,15 @@ import { TextInput } from "react-native-gesture-handler";
 type NavProps = NativeStackScreenProps<RootStackParamList, "UserEditorTool">;
 
 
-export default function UserEditorTool({ navigation }: NavProps, selectedUser: CustomUser) {
+export default function UserEditorTool({ navigation, route }: NavProps) {
+    const {selectedUser} = route.params;
+
     return(
         <View style={genericCss.container}>
             <Text>Dados de {selectedUser.name} {selectedUser.surename}</Text>
             <Text>e-mail: {selectedUser.eMail}</Text>
             <Text>permissão: {selectedUser.role.roleName}</Text>
-            <TextInput value={selectedUser.workplace}>local de trabalho:</TextInput>
+            <TextInput value={selectedUser.workplace}></TextInput>
         </View>
     )
 }
