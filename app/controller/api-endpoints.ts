8e277eff83be174c6d5bf83e69627b0a778f9941
RafaelBP02 <rafaelbertopereira@gmail.com>
2024-11-12 16:02:56 -0300

    feat: add new Request Method

--- app/controller/api-endpoints.ts
+++ app/controller/api-endpoints.ts
@@ -2,5 +2,6 @@ export const API_ENDPOINTS = {
     LOGIN: 'http://192.168.68.55:8080/auth/login',
     SIGNUP: 'http://192.168.68.55:8080/auth/signup',
     ADMIN_ALL_USERS: 'http://192.168.68.55:8080/users/all',
+    ADMIN_UPDATE_USER: 'http://192.168.68.55:8080/users/update',
   };
   
