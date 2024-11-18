f83ed297129bf0f67bc6b6a69ef322c3831f3613
RafaelBP02 <rafaelbertopereira@gmail.com>
2024-11-18 16:28:12 -0300

    feat: create notification config component

---
+++ app/views/notificationConfig.tsx
@@ -0,0 +1,114 @@
+import { colors, formCss } from "@/assets/css/FormsCss";
+import { genericCss } from "@/assets/css/GenericCss";
+import { RootStackParamList } from "@/constants/customTypes";
+import AuthContext from "@/contexts/Auth";
+import { Conversions } from "@/utils/Conversions";
+import React, { useContext, useEffect, useState } from "react";
+import {
+  Alert,
+  Button,
+  KeyboardAvoidingView,
+  Platform,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+import { NativeStackScreenProps } from "react-native-screens/lib/typescript/native-stack/types";
+
+type NavProps = NativeStackScreenProps<RootStackParamList, "Calculator">;
+
+
+export default function NotificationConfig({navigation}: NavProps) {
+  const {user} = useContext(AuthContext);
+
+  const [inicioExpediente, setInicioExpediente] = useState<string>("");
+  const [inicioIntervalo, setInicioIntervalo] = useState<string>("");
+  const [fimIntervalo, setFimIntervalo] = useState<string>("");
+  const [fimExpediente, setFimExpediente] = useState<string>("");
+
+  const handleNotificationConfig = () => {
+    const dataIE = Conversions.parseTimeString(inicioExpediente);
+    const dataII = Conversions.parseTimeString(inicioIntervalo);
+    const dataFI = Conversions.parseTimeString(fimIntervalo);
+    const dataFE = Conversions.parseTimeString(fimExpediente);
+
+    if (dataIE === null || dataII === null || dataFI === null || dataFE === null) {
+      Alert.alert(
+        "Formato de hora inválido",
+        "Por favor, apenas números, insira a hora no formato HH:MM."
+      );
+
+      return;
+    }
+
+    console.log("sucesso dados validados");
+
+    enviarHorarioConfigurado(dataIE, dataII, dataFI, dataFE);
+  }
+
+  const enviarHorarioConfigurado = async (
+    iExpediente: Date,
+    iIntervalo: Date,
+    fIntervalo: Date,
+    fExpediente: Date
+  ) => {
+    console.log("enviando dados salvos...")
+    //TODO: async API call 
+  };
+  
+
+  const limparHorarios = () => {
+    setInicioExpediente("");
+    setInicioIntervalo("");
+    setFimIntervalo("");
+    setFimExpediente("");
+  }
+
+  useEffect (() => {
+  }, [user]) 
+
+  return (
+    <KeyboardAvoidingView
+      behavior={Platform.OS == "ios" ? "padding" : "height"}
+    >
+      <View testID="calc-component" style={genericCss.container}>
+        <View style={formCss.formContainer}>
+          <Text style={formCss.formTitle}>Calculadora do Ponto - 8 horas</Text>
+          <View style={formCss.formItems}>
+            <TextInput
+              style={formCss.formInput}
+              value={inicioExpediente}
+              placeholder="Hora início expediente (HH:MM)"
+              onChangeText={(text) => setInicioExpediente(text)}
+              keyboardType="numeric"
+            />
+            <TextInput
+              style={formCss.formInput}
+              value={inicioIntervalo}
+              placeholder="Hora início intervalo (HH:MM)"
+              onChangeText={(text) => setInicioIntervalo(text)}
+              keyboardType="numeric"
+            />
+            <TextInput
+              style={formCss.formInput}
+              value={fimIntervalo}
+              placeholder="Hora fim intervalo (HH:MM)"
+              onChangeText={(text) => setFimIntervalo(text)}
+              keyboardType="numeric"
+            />
+            <TextInput
+            value={fimExpediente}
+            placeholder="Hora fim expediente (HH:MM)"
+            onChangeText={(text) => setFimExpediente(text)}
+            keyboardType="numeric"
+          />
+          </View>
+          <View style={formCss.button}>
+            <Button title="salvar" color={colors.primary} onPress={() => handleNotificationConfig()} />
+            <Button title="cancelar" color={colors.secondary} onPress={() => limparHorarios()} />
+          </View>
+        </View>
+      </View>
+    </KeyboardAvoidingView>
+  );
+}
