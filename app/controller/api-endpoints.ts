fe82e4dc8d455e8a246875f75263185ae72f0bd7
RafaelBP02 <rafaelbertopereira@gmail.com>
2024-11-21 16:31:15 -0300

    feat: add new request and endpoint

--- app/controller/api-endpoints.ts
+++ app/controller/api-endpoints.ts
@@ -3,6 +3,7 @@ export const API_ENDPOINTS = {
     SIGNUP: 'http://192.168.68.55:8080/auth/signup',
 
     SAVED_ALARM_CONFIG: 'http://192.168.68.55:8080/alarms',
+    SAVED_ALARM_CONFIG_BY_USER: 'http://192.168.68.55:8080/v2/alarms',
 
     ADMIN_ALL_USERS: 'http://192.168.68.55:8080/users/all',
     ADMIN_UPDATE_USER: 'http://192.168.68.55:8080/users/update',
--- app/controller/configsController.tsx
+++ app/controller/configsController.tsx
@@ -23,8 +23,8 @@ export class ConfigsController {
       method: "POST",
       headers: {
         Accept: "application/json",
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
       },
       body: JSON.stringify({
         workEntry: dataIE,
@@ -37,5 +37,55 @@ export class ConfigsController {
     });
 
     return await response.json();
+  }
+
+  static async updateWorktimeAlarmConfig(
+    token: string,
+    dataIE: string,
+    dataII: string,
+    dataFI: string,
+    dataFE: string,
+    userId: number
+  ): Promise<SavedAlarm> {
+    let response = await fetch(API_ENDPOINTS.SAVED_ALARM_CONFIG, {
+      method: "PUT",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({
+        workEntry: dataIE,
+        intervalBeginning: dataII,
+        intervalEnd: dataFI,
+        workEnd: dataFE,
+        workload: 8,
+        userId: userId,
+      }),
+    });
+
+    return await response.json();
+  }
+
+  static async findWorktimeAlarmConfig(token: string): Promise<SavedAlarm> {
+    try {
+      let response = await fetch(API_ENDPOINTS.SAVED_ALARM_CONFIG_BY_USER, {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status}`);
+      }
+
+      return await response.json();
+    } catch (error) {
+      console.error("Fetch data failed", error);
+      throw error;
+    }
   }
 }
